a5ce7499530fdb9ac8435b83e881d9cb041b04e7
Linus Marco <lmarco163@gmail.com>
2019-03-09 12:07:37 -0500

    add slider controls

---
+++ src/frontend/src/components/Slider.vue
@@ -0,0 +1,30 @@
+<template>
+    <b-container fluid>
+        <b-row class="my-1">
+            <b-col sm="2" class="text-right">
+                <span>{{ labelStart }}</span>
+            </b-col>
+            <b-col sm="8">
+                <b-form-input type="range" />
+            </b-col>
+            <b-col sm="2" class="text-left">
+                <span>{{ labelEnd }}</span>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+    name: 'Slider',
+    props: ['labelStart', 'labelEnd', 'range'],
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style scoped>
+#slider {
+}
+</style>
