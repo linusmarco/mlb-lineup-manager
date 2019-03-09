c7224412ee78c5eed57e9951e706684a495e0e97
Linus Marco <lmarco163@gmail.com>
2019-03-09 13:30:16 -0500

    frontend changes and py reqs

--- src/frontend/src/components/Roster.vue
+++ src/frontend/src/components/Roster.vue
@@ -1,17 +1,42 @@
 <template>
     <div id="roster">
-        ROSTER
+        <div id="roster-table-container">
+            <h3>
+                Full Roster
+            </h3>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>&nbsp;</th>
+                        <th>Name</th>
+                        <th>Position</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(p, i) in players" :key="p.id">
+                        <td>{{ i + 1 }}</td>
+                        <td>{{ p.name }}</td>
+                        <td>{{ p.pos }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Roster',
-    props: {
-        // msg: String
+    props: ['players'],
+    data() {
+        return {};
     }
 };
 </script>
 
 <style scoped>
+#roster-table-container {
+    width: 100%;
+    margin: auto;
+}
 </style>
